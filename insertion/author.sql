e755d69279a981f17f369dacb5ba57d250260cc6
Josh Birdwell <jbirdwell34@gmail.com>
2017-10-02 09:43:36 -0500

    Fix some issues

--- insertion/author.sql
+++ insertion/author.sql
@@ -1,8 +1,8 @@
-insert into Author values(1, "Morgan", 60, 26);
-insert into Author values(2, "Cruz", 38, 8);
-insert into Author values(3, "Adams", 30, 5);
-insert into Author values(4, "Perry", 65, 24);
-insert into Author values(5, "Derek", 50, 10);
-insert into Author values(6, "Derek", 32, 4);
-insert into Author values(7, "Lewis", 70, 20);
-insert into Author values(8, "Alice", 47, 7);
+insert into Author values(1, 'Morgan', 60, 26);
+insert into Author values(2, 'Cruz', 38, 8);
+insert into Author values(3, 'Adams', 30, 5);
+insert into Author values(4, 'Perry', 65, 24);
+insert into Author values(5, 'Derek', 50, 10);
+insert into Author values(6, 'Derek', 32, 4);
+insert into Author values(7, 'Lewis', 70, 20);
+insert into Author values(8, 'Alice', 47, 7);
