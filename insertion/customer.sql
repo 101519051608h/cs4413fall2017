4f497d7a0079df60690e89f92547a0ad64116a87
Josh Birdwell <jbirdwell34@gmail.com>
2017-10-15 20:18:18 -0500

    Remove others

--- insertion/customer.sql
+++
@@ -1,5 +0,0 @@
-insert into Customer values(200, 'John', 10, 'Children');
-insert into Customer values(201, 'Emily', 7 , 'Adult');
-insert into Customer values(202, 'Ashely', 19, 'Adult');
-insert into Customer values(203, 'Edward', 3, 'Children');
-insert into Customer values(204, 'Wu', 12, 'Adult');
